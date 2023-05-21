f51e32e367138621a13505f56cc0bbb2b0a6ed32
Kelan Ige <113405702+kelanige@users.noreply.github.com>
2023-05-20 20:42:57 -0700

    Build Linux kernel

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script outline to install and build kernel.
-# Author: Siddhant Jajoo.
+# Original Author: Siddhant Jajoo.
+# Author: Kelan Ige.
 
 set -e
 set -u
@@ -34,10 +35,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc --all) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc --all) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j$(nproc --all) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,34 +54,63 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir -p rootfs/bin rootfs/dev rootfs/etc rootfs/home rootfs/lib rootfs/lib64 rootfs/proc rootfs/sbin rootfs/sys rootfs/tmp rootfs/usr rootfs/var
+mkdir -p rootfs/usr/bin rootfs/usr/lib rootfs/usr/sbin
+mkdir -p rootfs/var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
-    cd busybox
+    pushd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
-    cd busybox
+    pushd busybox
 fi
 
-# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+popd
 
+pushd rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+TOOLCHAIN_DIR=`${CROSS_COMPILE}gcc --print-sysroot`
+cp ${TOOLCHAIN_DIR}/lib/ld-linux-aarch64.so.1 lib/
+cp ${TOOLCHAIN_DIR}/lib64/libm.so.6 lib64/
+cp ${TOOLCHAIN_DIR}/lib64/libresolv.so.2 lib64/
+cp ${TOOLCHAIN_DIR}/lib64/libc.so.6 lib64/
 
-# TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+popd
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+if [ ! -d "${OUTDIR}/assignments-3-and-later-kelanige" ]
+then
+    git clone https://github.com/cu-ecen-aeld/assignments-3-and-later-kelanige.git
+    pushd assignments-3-and-later-kelanige/finder-app
+else
+    pushd assignments-3-and-later-kelanige/finder-app
+fi
 
-# TODO: Chown the root directory
+make clean
+make writer
 
-# TODO: Create initramfs.cpio.gz
+cp writer ${OUTDIR}/rootfs/home/
+cp finder.sh ${OUTDIR}/rootfs/home/
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp --parents conf/username.txt ${OUTDIR}/rootfs/home/
+popd
+
+sudo chown -R root:root ${OUTDIR}/rootfs
+
+pushd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+popd
+gzip -f initramfs.cpio
