5b98a092ae557fcea2ebcfa418134c42d8fcefd3
Kelan Ige <113405702+kelanige@users.noreply.github.com>
2023-07-17 20:33:22 -0700

    Added threads to handle connection.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,8 @@
 #include <sys/types.h>
 #include <syslog.h>
 #include <unistd.h>
+
+#include "queue.h"
 
 struct Parameters
 {
@@ -20,6 +23,42 @@ struct Parameters
 };
 
 struct Parameters parameters;
+
+struct Node
+{
+    SLIST_ENTRY(Node)
+    next;
+    pthread_t thread;
+    FILE *log;
+    pthread_mutex_t *mutex;
+    int peer;
+    int complete;
+    int exit;
+};
+
+SLIST_HEAD(NodeHead, Node);
+struct NodeHead thread_list;
+
+// Inserts a new entry into a list and returns a pointer to the inserted Node.
+struct Node *insert_node(struct NodeHead *head)
+{
+    struct Node *new_node = malloc(sizeof(struct Node));
+    memset(new_node, 0, sizeof(struct Node));
+    if (SLIST_EMPTY(head))
+    {
+        SLIST_INSERT_HEAD(head, new_node, next);
+    }
+    else
+    {
+        struct Node *current = SLIST_FIRST(head);
+        while (SLIST_NEXT(current, next) != NULL)
+        {
+            current = SLIST_NEXT(current, next);
+        }
+        SLIST_INSERT_AFTER(current, new_node, next);
+    }
+    return new_node;
+}
 
 int init()
 {
@@ -35,19 +74,116 @@ int init()
     return 0;
 }
 
+void *handle_connection(void *arg)
+{
+    struct Node *parameters = (struct Node *)arg;
+    const int MAX_CHUNK = 1024;
+    printf("handling_connection...\n");
+    // Read data.
+    char chunk[MAX_CHUNK];
+    int bytes = 0;
+    while (parameters->exit == 0 && (bytes = recv(parameters->peer, chunk, MAX_CHUNK - 1, 0)) > 0)
+    {
+        chunk[bytes] = '\0';
+
+        // Acquire mutex.
+        pthread_mutex_lock(parameters->mutex);
+
+        // Write line to output file.
+        if (parameters->log != NULL)
+        {
+            fputs(chunk, parameters->log);
+        }
+
+        // Release mutex.
+        pthread_mutex_unlock(parameters->mutex);
+
+        // Break if newline was found.
+        if (strpbrk(chunk, "\n") != NULL)
+        {
+            break;
+        }
+    }
+
+    // Zero buffer for reuse.
+    memset(chunk, 0, sizeof(char) * MAX_CHUNK);
+
+    // Save file position.
+    const long int output_position = ftell(parameters->log);
+    if (output_position == -1)
+    {
+        perror("Failed to get file position");
+        exit(-1);
+    }
+
+    // Acquire mutex.
+    pthread_mutex_lock(parameters->mutex);
+
+    // Seek to beginning of file.
+    if (fseek(parameters->log, 0, SEEK_SET) != 0)
+    {
+        perror("Failed to seek to beginning of log file");
+        exit(-1);
+    }
+
+    // Echo back data.
+    while (parameters->exit == 0 && fgets(chunk, MAX_CHUNK, parameters->log) != NULL)
+    {
+        char *end = strrchr(chunk, '\0');
+        if (end == NULL)
+        {
+            break;
+        }
+        if (send(parameters->peer, chunk, (size_t)(end - chunk), 0) == -1)
+        {
+            perror("Call to send failed");
+            break;
+        }
+        memset(chunk, 0, sizeof(char) * MAX_CHUNK);
+    }
+
+    // Release mutex.
+    pthread_mutex_unlock(parameters->mutex);
+
+    // Set completed.
+    parameters->complete = 1;
+
+    // Exit thread.
+    pthread_exit(arg);
+}
+
+void join_completed_threads(int req_exit)
+{
+    struct Node *current, *tmp;
+    SLIST_FOREACH_SAFE(current, &thread_list, next, tmp)
+    {
+        current->exit = 1;
+        if (current->complete == 1)
+        {
+            pthread_join(current->thread, NULL);
+            // Close peer connection.
+            if (close(current->peer) == -1)
+            {
+                perror("Call to close peer socket failed");
+                exit(-1);
+            }
+            SLIST_REMOVE(&thread_list, current, Node, next);
+            free(current);
+        }
+    }
+}
+
 int cleanup()
 {
     // Set stop.
     parameters.run = 0;
 
-    // Shutdown peer socket.
-    if (parameters.peer_fd != 0)
+    // Shutdown peer sockets.
+    do
     {
-        if (shutdown(parameters.peer_fd, SHUT_RDWR) == -1)
-        {
-            perror("Call to shutdown failed");
-        }
-    }
+        join_completed_threads(1);
+    } while (!SLIST_EMPTY(&thread_list));
+    
     // Shutdown server socket.
     if (parameters.serv_fd != 0)
     {
@@ -56,15 +192,7 @@ int cleanup()
             perror("Call to shutdown failed");
         }
     }
-    // Close peer socket.
-    if (parameters.peer_fd != 0)
-    {
-        if (close(parameters.peer_fd) == -1)
-        {
-            perror("Call to close failed");
-        }
-        parameters.peer_fd = 0;
-    }
+
     // Close server socket.
     if (parameters.serv_fd != 0)
     {
@@ -74,6 +202,7 @@ int cleanup()
         }
         parameters.serv_fd = 0;
     }
+
     // Close output file.
     if (parameters.output_fd != NULL)
     {
@@ -83,6 +212,7 @@ int cleanup()
         }
         parameters.output_fd = NULL;
     }
+
     // Delete /var/tmp/aesdsocketdata.
     if (access("/var/tmp/aesdsocketdata", F_OK) == 0)
     {
@@ -91,6 +221,7 @@ int cleanup()
             perror("Call to remove failed");
         }
     }
+
     // Close syslog.
     closelog();
     return 0;
@@ -104,8 +235,7 @@ void sigint_handler(int signum)
 
 int main(int argc, char *argv[])
 {
-    const int MAX_CHUNK = 1024;
-    const int MAX_CONNECTIONS = 1;
+    const int MAX_CONNECTIONS = 5;
 
     // Register signal handlers.
     struct sigaction handler;
@@ -172,13 +302,22 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
+    // Open output file.
+    parameters.output_fd = fopen("/var/tmp/aesdsocketdata", "a+");
+    if (parameters.output_fd == NULL)
+    {
+        perror("Failed to open output file");
+        exit(-1);
+    }
+    pthread_mutex_t log_mutex;
+
     while (parameters.run)
     {
         // Accept an incoming connection.
         struct sockaddr_in peer_addr;
         socklen_t peer_len = sizeof(peer_addr);
-        parameters.peer_fd = accept(parameters.serv_fd, (struct sockaddr *)&peer_addr, &peer_len);
-        if (parameters.peer_fd == -1)
+        const int peer = accept(parameters.serv_fd, (struct sockaddr *)&peer_addr, &peer_len);
+        if (peer == -1)
         {
             perror("Call to accept failed");
             exit(-1);
@@ -190,83 +329,16 @@ int main(int argc, char *argv[])
             exit(-1);
         }
         syslog(LOG_INFO, "Acceped connection from %s", peer_ip);
-        // Open output file.
-        parameters.output_fd = fopen("/var/tmp/aesdsocketdata", "a+");
-        if (parameters.output_fd == NULL)
-        {
-            perror("Failed to open output file");
-            exit(-1);
-        }
-        // Read data.
-        char chunk[MAX_CHUNK];
-        int bytes = 0;
-        int total_bytes = 0;
-        while ((bytes = recv(parameters.peer_fd, chunk, MAX_CHUNK - 1, 0)) > 0)
-        {
-            chunk[bytes] = '\0';
-
-            // Write line to output file.
-            if (parameters.output_fd != NULL)
-            {
-                fputs(chunk, parameters.output_fd);
-                total_bytes += bytes;
-            }
-
-            // Break if newline was found.
-            if (strpbrk(chunk, "\n") != NULL)
-            {
-                break;
-            }
-        }
-        if (fclose(parameters.output_fd) == EOF)
-        {
-            perror("Call to close file failed");
-            exit(-1);
-        }
-        parameters.output_fd = 0;
-
-        // Zero buffer for reuse.
-        memset(chunk, 0, sizeof(char) * MAX_CHUNK);
-
-        // Open file in read mode.
-        parameters.output_fd = fopen("/var/tmp/aesdsocketdata", "r");
-        if (parameters.output_fd == NULL)
-        {
-            perror("Failed to open data file in read mode");
-            exit(-1);
-        }
-
-        // Echo back data.
-        while (fgets(chunk, MAX_CHUNK, parameters.output_fd) != NULL)
-        {
-            char *end = strrchr(chunk, '\0');
-            if (end == NULL)
-            {
-                break;
-            }
-            if (send(parameters.peer_fd, chunk, (size_t)(end - chunk), 0) == -1)
-            {
-                perror("Call to send failed");
-                break;
-            }
-            memset(chunk, 0, sizeof(char) * MAX_CHUNK);
-        }
-
-        // Close output file.
-        if (fclose(parameters.output_fd) == EOF)
-        {
-            perror("Call to close file failed");
-            exit(-1);
-        }
-        parameters.output_fd = 0;
-
-        // Close peer connection.
-        if (close(parameters.peer_fd) == -1)
-        {
-            perror("Call to close peer socket failed");
-            exit(-1);
-        }
-        parameters.peer_fd = 0;
+        struct Node *node = insert_node(&thread_list);
+        node->log = parameters.output_fd;
+        node->mutex = &log_mutex;
+        node->peer = peer;
+        node->complete = 0;
+        node->exit = 0;
+        pthread_create(&node->thread, NULL, handle_connection, (void *)node);
+        join_completed_threads(0);
     }
+
+    cleanup();
     exit(EXIT_SUCCESS);
 }
